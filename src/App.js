30128840f8d9905e2d731dbda3ac8ee919f550b9
Ritik Sinha <ritik210798@gmail.com>
2023-07-15 23:46:27 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar/Navbar"
+import "./App.css"
+// import MainContent from "./components/ContentContainer/MainContent"
+import Hero from "./components/Hero/hero"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
